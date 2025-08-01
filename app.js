f8fe9467eda5acf2fbf9693353b49929f864b86a
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-08-01 16:31:12 +0530

    first commit

--- app.js
+++ app.js
@@ -15,3 +15,27 @@ videos.forEach((video) => {
   video.addEventListener("mouseenter", () => video.play());
   video.addEventListener("mouseleave", () => video.pause());
 });
+
+const searchInput = document.querySelector(".searchInput");
+const noResults = document.querySelector(".no-results");
+
+if (searchInput) {
+  searchInput.addEventListener("input", () => {
+    const Searchvalue = searchInput.value.toLowerCase().trim();
+    const productCards = document.querySelectorAll(".product-card");
+
+    productCards.forEach((card) => {
+      const title = card
+        .querySelector(".product-title")
+        .textContent.toLowerCase();
+
+      if (title.includes(Searchvalue)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+
+    noResults.style.display = found ? "none" : "block";
+  });
+}
